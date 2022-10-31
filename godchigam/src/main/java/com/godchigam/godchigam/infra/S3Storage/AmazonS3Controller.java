c443ff594c88ad8f9ed5951423b412ad41499250
hyobinkim <hyobim0911@gmail.com>
2022-10-31 15:06:56 +0900

    Feat : #12 image 업로드 로직 추가

---
+++ godchigam/src/main/java/com/godchigam/godchigam/infra/S3Storage/AmazonS3Controller.java
@@ -0,0 +1,26 @@
+package com.godchigam.godchigam.infra.S3Storage;
+
+import com.godchigam.godchigam.global.common.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/image")
+public class AmazonS3Controller {
+
+    private final AwsS3Service awsS3Service;
+
+    /**
+     * Amazon S3에 파일 업로드
+     * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환
+     */
+    @PostMapping("")
+    public CommonResponse uploadFile(@RequestPart List<MultipartFile> multipartFile, @RequestParam String filePath) {
+        return CommonResponse.success(awsS3Service.uploadFile(multipartFile,filePath),"이미지 업로드 성공");
+    }
+
+}
